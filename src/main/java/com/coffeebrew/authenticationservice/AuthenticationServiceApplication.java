ce8d609c022d0e134d7609012eacfc747ca0117e
Sourav Kabiraj <sourav.kabiraj@outlook.com>
2020-07-18 19:32:02 +0530

    #AUT-3 | Sourav | Introduced service discovery.

--- src/main/java/com/coffeebrew/authenticationservice/AuthenticationServiceApplication.java
+++ src/main/java/com/coffeebrew/authenticationservice/AuthenticationServiceApplication.java
@@ -2,10 +2,12 @@ package com.coffeebrew.authenticationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.coffeebrew.authenticationservice")
+@EnableDiscoveryClient
 public class AuthenticationServiceApplication {
 
     public static void main(String[] args) {
