cf61ba9e5b27a0e50fa1c921b0d4826c5b734305
Sourav Kabiraj <sourav.kabiraj@outlook.com>
2020-07-19 16:01:37 +0530

    #AUT-4 | Sourav | Configured inter-service call via gateway.

--- src/main/java/com/coffeebrew/authenticationservice/gateways/UserServiceGateway.java
+++ src/main/java/com/coffeebrew/authenticationservice/gateways/UserServiceGateway.java
@@ -7,16 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "gateway-server")
 @RibbonClient(name = "user-service")
 public interface UserServiceGateway {
 
-    @PostMapping("/users")
+    @PostMapping("/user-service/users")
     public ResponseEntity<User> save(@RequestBody User user);
 
-    @GetMapping(path = "/users/{id}")
+    @GetMapping(path = "/user-service/users/{id}")
     public ResponseEntity<User> getById(@PathVariable String id);
 
-    @GetMapping("/users")
+    @GetMapping("/user-service/users")
     public ResponseEntity<User> getByName(@RequestParam("name") String name);
 }
